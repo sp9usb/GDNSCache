7c46480fe22e06199c9a6c99f7a128184cf62876
Giskard Solutions <kamil4521@gmail.com>
2014-12-08 19:14:21 +0100

    Removed bad DNS servers.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = exports = {
-  DNS_SERVER: ['8.8.8.8', '8.8.4.4', '4.2.2.1', '194.204.159.1'],
+  DNS_SERVER: ['8.8.8.8', '194.204.159.1'],
   DNS_PORT: 53,
   TIME_TO_REMOVE_RECORD_FROM_CACHE: 86400,
   ENABLE_DEBUG: true
